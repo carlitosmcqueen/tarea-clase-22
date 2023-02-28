e30149613755a90fe7f5d507ec267e3c4ad7c401
Carlos Quispe <carlosqe2000@gmail.com>
2023-02-28 13:35:30 -0300

    rayway intento 4

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -4,14 +4,14 @@ import { Router } from 'express'
 //nuevo intento 
 import passport from '../utils/passport.js'
 
-import twilio from 'twilio';
+//import twilio from 'twilio';
 import * as dotenv from "dotenv"
 dotenv.config()
 
 //twilio
 const accountSID = "AC68263a028427d38a73d6e3d832cdc0ae"
 const authToken = process.env.TWILIO
-const client = twilio(accountSID, authToken)
+//const client = twilio(accountSID, authToken)
 
 const router = Router()
 const app = express()
